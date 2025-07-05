5e92844ae03e728d4638f7de5a50dff8f5e905cb
Vakacharla-Lokesh <lokeshgpta04@gmail.com>
2025-07-05 17:56:09 +0530

    final changes

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
   try {
-    const { userId } = getAuth();
+    const { userId } = getAuth(request);
     const { address, items } = await request.json();
 
     if (!address || items.length === 0) {
@@ -15,7 +15,7 @@ export async function POST(request) {
 
     const amount = await items.reduce(async (acc, item) => {
       const product = await Product.findById(item.product);
-      return acc + product.offerPrice * item.quantity;
+      return await acc + product.offerPrice * item.quantity;
     }, 0);
 
     await inngest.send({
